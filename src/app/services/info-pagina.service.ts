4b9e498f40fc5d659a41fcfe88492f7d8af27d4b
Daniel Quiñones Lopez <dquinoneslopez@gmail.com>
2018-10-10 16:28:58 +0200

    Fin de la Sección 5

--- src/app/services/info-pagina.service.ts
+++ src/app/services/info-pagina.service.ts
@@ -9,16 +9,28 @@ export class InfoPaginaService {
 
   info: InfoPagina = {};
   cargada = false;
+  equipo: any[] = [];
 
   constructor(private http: HttpClient) {
-    console.log('Servicio de InfoPagina listo.');
+    this.cargarInfo();
+    this.cargarEquipo();
+   }
 
+  private cargarInfo() {
     // Leer json
     this.http.get('assets/data/data-pagina.json')
              .subscribe((resp: InfoPagina) => {
-               this.cargada = true;
-               this.info = resp;
+                this.cargada = true;
+                this.info = resp;
                 // console.log(resp);
+              });
+  }
+
+  private cargarEquipo() {
+    this.http.get('https://angular-html-89d1e.firebaseio.com/equipo.json')
+             .subscribe((resp: any[]) => {
+               this.equipo = resp;
+               console.log(resp);
              });
-   }
+  }
 }
